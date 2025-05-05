b32971349aed65eb0d2a5b860d344351ff8d1124
fck <fck@FREDERIK>
2025-05-06 01:45:29 +0200

    Add project files.

---
+++ image-reading-test/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tesseract;
+
+string text;
+using (var engine = new TesseractEngine(@"C:\Users\fck\source\repos\image-reading-test\image-reading-test\TessData",
+    "eng",
+    EngineMode.Default))
+{
+    using Pix? image = Pix.LoadFromFile(@"C:\Users\fck\source\repos\image-reading-test\image-reading-test\test-plate.jpg");
+    using Page? page = engine.Process(image);
+    text = page.GetText().Trim();
+}
+
+string textNoWhiteSpace = String.Concat(text.Where(c => !Char.IsWhiteSpace(c)));
+
+Console.WriteLine(textNoWhiteSpace);
+
+//File.WriteAllText(@"C:\output.txt", text);
